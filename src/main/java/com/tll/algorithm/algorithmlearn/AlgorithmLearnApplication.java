42a788683f936056fe0ebf9cbac8c7300389d2e4
tianliangliang <tianliangliang@mobike.com>
2018-01-28 23:35:30 +0800

    init

---
+++ src/main/java/com/tll/algorithm/algorithmlearn/AlgorithmLearnApplication.java
@@ -0,0 +1,12 @@
+package com.tll.algorithm.algorithmlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmLearnApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tll/algorithm/algorithmlearn/AlgorithmLearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tll.algorithm.algorithmlearn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgorithmLearnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
